979dd2bd5b12179a6d882b8a97e18b4c0c54cd24
Jonathan Perry <jonathanperry@sequencemedia.net>
2021-12-28 15:43:05 +0000

    Queue

--- src/index.mjs
+++ src/index.mjs
@@ -13,10 +13,13 @@ const log = debug('@sequencemedia/watch-match')
 const error = debug('@sequencemedia/watch-match:error')
 const info = debug('@sequencemedia/watch-match:info')
 
-function getMatch (from, to) {
+const DELAY = (3 * (60 * 1000))
+
+function getMatch (from, to, delay) {
   info('getMatch')
 
   const pattern = new RegExp(from, 'g')
+  const map = new Map()
 
   return async function match (filePath) {
     info('match')
@@ -24,11 +27,21 @@ function getMatch (from, to) {
     const fileData = await readFile(filePath, 'utf8')
 
     if (pattern.test(fileData)) {
-      log(`Writing "${filePath}"`)
+      if (map.has(filePath)) {
+        log(`Clearing "${filePath}" ...`)
 
-      return (
-        await writeFile(filePath, fileData.replace(pattern, to), 'utf8')
-      )
+        clearTimeout(map.get(filePath))
+      }
+
+      log(`Queueing "${filePath}" ...`)
+
+      map.set(filePath, setTimeout(async function write () {
+        log(`Writing "${filePath}"`)
+
+        return (
+          await writeFile(filePath, fileData.replace(pattern, to), 'utf8')
+        )
+      }, delay))
     }
   }
 }
@@ -37,10 +50,10 @@ function handleError ({ message }) {
   error(`Error in watcher: "${message}"`)
 }
 
-export default function watchMatch (from, to, path) {
+export default function watchMatch (from, to, path, delay = DELAY) {
   log('watchMatch')
 
-  const match = getMatch(from, to)
+  const match = getMatch(from, to, delay)
 
   return (
     chokidar.watch(normalise(path))
