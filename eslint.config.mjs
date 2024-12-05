60fb89e866b22ce9ac957ab32205e06f1a29c3b9
Jonathan Perry <jonathanperry@sequencemedia.net>
2024-12-05 18:30:38 +0000

    `@sequencemedia/eslint-config-standard`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,9 @@
 import merge from '@sequencemedia/eslint-config-standard/merge'
-import parser from '@babel/eslint-parser'
 import globals from 'globals'
 
 export default (
   merge({
     languageOptions: {
-      parser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-      },
       globals: {
         ...globals.node
       }
